970f2a953830b3f699f3d71f446c4e4de6bb5f03
Contra07 <nano2012rus@yandex.ru>
2024-03-29 11:49:07 +0400

    Новый скрипт

--- script.py
+++ script.py
@@ -2,11 +2,21 @@ import subprocess
 import re
 import csv
 
-exe_path = "taskbag-uname -m.exe"
 
-with open("output.csv", "w", newline='') as output_file:
-    writer = csv.writer(output_file)
-    for _ in range(2):
-        result = subprocess.run([exe_path], capture_output=True, text=True)
-        numbers = re.findall(r'\d+\.\d+|\d+', result.stdout)
-        writer.writerow(numbers)
+p = 12
+s = 5
+n = 10
+m = 10000
+
+i = 100000
+while i <= pow(10, n):
+    exe_path = "taskbag.exe"
+    with open("out/output_" + str(s) + ".csv", "w", newline='') as output_file:
+        writer = csv.writer(output_file)
+        print(i)
+        for _ in range(m):
+            result = subprocess.run([exe_path, str(i), str(p)], capture_output=True, text=True)
+            numbers = re.findall(r'\d+\.\d+|\d+', result.stdout)
+            writer.writerow(numbers)
+        s += 1
+        i = pow(10, n)
